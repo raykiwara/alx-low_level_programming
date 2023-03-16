21e6bcabaa3d84baa65d1329402a2464e4c92558
raykiwara <raykiwara10@gmail.com>
2023-03-16 20:20:10 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main fuction
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the output is a piece of art,\n");
+	return (0);
+}
+
