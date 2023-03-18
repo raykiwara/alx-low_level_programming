7fd78772f3d55174465adfa8281014b1bcc34c2f
raykiwara <raykiwara10@gmail.com>
2023-03-18 19:52:50 +0000

    changes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("%d is positive", n);
 	else if (n == 0)
 		printf("%d is zero", n);
-	else
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	printf("\n");
 	return (0);
