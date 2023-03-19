0f4aaaee861781d349c4a138ed996973796f4d22
raykiwara <raykiwara10@gmail.com>
2023-03-19 14:54:30 +0000

    numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: zero
+ */
+int main(void)
+{
+char i;
+
+
+for (i = 0; i <= 9; i++)
+{
+	putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
