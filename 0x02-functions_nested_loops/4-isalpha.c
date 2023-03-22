fe805f292cb4a6b8e4613d1470791387c049ea97
raykiwara <raykiwara10@gmail.com>
2023-03-22 09:20:42 +0000

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - prints alphabet
+ * @c: character parameter
+ * Return: 1 if alphabet 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else 
+		return (0);
+}
