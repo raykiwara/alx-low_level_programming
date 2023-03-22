f131a390c7375ebd1c3fcf6fafe7f4b44d79da44
raykiwara <raykiwara10@gmail.com>
2023-03-22 12:12:08 +0000

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints sign
+ * @n: function parameter
+ * Return: 0 if equal to zero, 1 if positive, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
