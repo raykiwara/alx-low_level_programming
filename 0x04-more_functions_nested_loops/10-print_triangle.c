0092d360ec05c688917f415c6f7bf49993280acd
raykiwara <raykiwara10@gmail.com>
2023-03-24 22:20:26 +0000

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,17 +8,22 @@ void print_triangle(int size)
 	int i, j, k;
 
 	if (size <= 0)
-		_putchar('\n');
-	for (i = 0; i <= size; i++)
 	{
-		for (j = size - i; j >= 1; j--)
-		{
-			_putchar(' ');
-		}
-		for (k = 1; k <= i; k++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (j = size - i; j >= 1; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
