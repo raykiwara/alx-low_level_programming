3dd0741b1f75521d8d1e992ece651c0a521d8d01
raykiwara <raykiwara10@gmail.com>
2023-03-24 19:20:20 +0000

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - draws triangle
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i <= size; i++)
+	{
+		for (j = size - i; j >= 1; j--)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
