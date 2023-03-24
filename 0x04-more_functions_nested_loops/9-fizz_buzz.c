9619d338343773415d70f645b0df1ac819780ab9
raykiwara <raykiwara10@gmail.com>
2023-03-24 17:43:00 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - fizzbuzz problem
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%i ", i);
+	}
+	printf("\n");
+	return (0);
+}
