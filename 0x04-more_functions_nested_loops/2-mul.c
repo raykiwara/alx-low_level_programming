758ea43661e8f16a9236fdf606609a0a84868383
raykiwara <raykiwara10@gmail.com>
2023-03-24 13:30:29 +0000

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - prints multiple of two numbers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: multiple
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
