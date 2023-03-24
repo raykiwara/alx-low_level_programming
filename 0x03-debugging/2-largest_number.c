a58778e67570dc4b69c0933cc3cc1de35a86f72d
raykiwara <raykiwara10@gmail.com>
2023-03-24 20:18:58 +0000

    leap year

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,19 +17,25 @@ int largest_number(int a, int b, int c)
 		largest = a;
 	}
 	else if (a > c && c > b)
+	{
 		largest = a;
+	}
 	else if (b > a && a > c)
 	{
 		largest = b;
 	}
 	else if (b > c && c > b)
+	{
 		largest = b;
+	}
 	else if (c > a && a > b)
 	{
 		largest = c;
 	}
 	else if (c > b && b > a)
+	{
 		largest = c;
+	}
 
 	return (largest);
 }
