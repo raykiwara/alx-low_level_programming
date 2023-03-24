1185fb2a54ea36a1b50aa3dc025f8e2c6327a939
raykiwara <raykiwara10@gmail.com>
2023-03-24 22:04:26 +0000

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
 			printf("%i ", i);
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
