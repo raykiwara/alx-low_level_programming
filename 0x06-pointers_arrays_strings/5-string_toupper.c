d9ee8b01871a348f9e4822fdf24ca142728afea9
raykiwara <raykiwara10@gmail.com>
2023-03-31 20:32:49 +0000

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - converts a string to upper case
+ * @str: string
+ * Return: str in upper
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
