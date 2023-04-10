9a20a107971f264e9c3e78aee6cf311237f4635b
raykiwara <raykiwara10@gmail.com>
2023-04-10 20:53:52 +0000

    args c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints no. of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	
+	if (argv[i] >= argv[0])
+	{
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
