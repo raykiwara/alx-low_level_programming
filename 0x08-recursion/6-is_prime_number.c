d01706f4aec3ee25cf1f08cc901afbda62886e78
raykiwara <raykiwara10@gmail.com>
2023-06-07 21:05:13 +0000

    isprime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,13 +6,14 @@
  */
 int is_prime_number(int n)
 {
-	int i;
+	int i = 2;
 
-	if (n < 2)
+	if (n == 0 || n == 1)
 		return (0);
+	if (n == i)
+		return (1);
 	if (n % i == 0)
 		return (0);
-	if (i * i > n)
-		return (1);
-	return (is_prime_number(n, i + 1));
+	return (is_prime_number(n));
+	i++;
 }
