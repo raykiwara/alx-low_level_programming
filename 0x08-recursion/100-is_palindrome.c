7ec148f182c37799e41464960b6362e9848c2240
raykiwara <raykiwara10@gmail.com>
2023-06-17 12:56:11 +0000

    Recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/*
+ *
+ */
+int _strlen(char *s, int i);
+
+int is_palindrome(char *s)
+{
+	int j;
+
+	*s(_strlen) = j;
+
+	if (s[0] == 0)
+		return (1);
+	if (s[0] != s[j])
+		return (0);
+	if (s[0] == s[i] && is_palindrome(s[0 + 1] == s[j - 1]))
+		return (1);
+
+}
+
+int _strlen(char *s, int i)
+{
+	if (s[i] == '\0')
+		return (i);
+	_strlen(s, ++i);
+	return (_strlen);
+}		
