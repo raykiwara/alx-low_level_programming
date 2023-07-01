bc5fe19390447d0b941dcb7defdd62a08384ac06
raykiwara <raykiwara10@gmail.com>
2023-07-01 12:53:48 +0000

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees list_t
+ * @head; pointer to the first node
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
