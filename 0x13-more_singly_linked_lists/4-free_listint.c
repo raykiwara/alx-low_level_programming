b470a986026bd8b9841c68175a70cdad62694a16
raykiwara <raykiwara10@gmail.com>
2023-07-04 19:26:29 +0000

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - frees a listint_t list
+ * @head: head node
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
